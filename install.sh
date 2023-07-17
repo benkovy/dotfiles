4d5de671a32cd739902f178ecd3e34fb084909ea
Ben Kovacs <ben.kovacs@shopify.com>
2023-07-17 16:05:38 -0300

    is it installing oh-my-zsh that breaks things?

--- install.sh
+++ install.sh
@@ -19,51 +19,51 @@ fi
 #   git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 # fi
 
-# HOMEDIR=$HOME
-# BACKUPDIR="$HOMEDIR/.dotfiles.backups"
-# CURRENTDIR=$(pwd -P)
-# DOTFILEDIR="$CURRENTDIR/dotfiles"
+HOMEDIR=$HOME
+BACKUPDIR="$HOMEDIR/.dotfiles.backups"
+CURRENTDIR=$(pwd -P)
+DOTFILEDIR="$CURRENTDIR/dotfiles"
 
-# backup_dotfile() {
-#   if [[ -e "$HOMEDIR/$1" ]]; then
+backup_dotfile() {
+  if [[ -e "$HOMEDIR/$1" ]]; then
 
-#     # Do we already have any backups? How many?
-#     # This needs some work. Doesn't handle things like
-#     # $BACKUPDIR/.vim* and $BACKUPDIR/.vimrc* well
-#     dotfile_count=$(find $BACKUPDIR/$1* -maxdepth 0 2> /dev/null | wc -l | sed 's/ //g')
+    # Do we already have any backups? How many?
+    # This needs some work. Doesn't handle things like
+    # $BACKUPDIR/.vim* and $BACKUPDIR/.vimrc* well
+    dotfile_count=$(find $BACKUPDIR/$1* -maxdepth 0 2> /dev/null | wc -l | sed 's/ //g')
 
-#     if [[ $dotfile_count -ne '0' ]]; then
-#       mv $HOMEDIR/$1 $BACKUPDIR/$1.$dotfile_count
-#     else
-#       mv $HOMEDIR/$1 $BACKUPDIR/$1
-#     fi
-#   fi
-# }
+    if [[ $dotfile_count -ne '0' ]]; then
+      mv $HOMEDIR/$1 $BACKUPDIR/$1.$dotfile_count
+    else
+      mv $HOMEDIR/$1 $BACKUPDIR/$1
+    fi
+  fi
+}
 
-# symlink_dotfile() {
-#   ln -sf $DOTFILEDIR/$1 $HOMEDIR/$1
-# }
+symlink_dotfile() {
+  ln -sf $DOTFILEDIR/$1 $HOMEDIR/$1
+}
 
 
-# if [[ ! -e $BACKUPDIR ]]; then
-#   echo "Creating back ups folder $BACKUPDIR..."
-#   mkdir $BACKUPDIR
-# fi
+if [[ ! -e $BACKUPDIR ]]; then
+  echo "Creating back ups folder $BACKUPDIR..."
+  mkdir $BACKUPDIR
+fi
 
-# dotfiles=$(ls -1 -A $DOTFILEDIR 2> /dev/null)
+dotfiles=$(ls -1 -A $DOTFILEDIR 2> /dev/null)
 
-# if [[ $dotfiles ]]; then
-#   echo "Symlinking dotfiles..."
+if [[ $dotfiles ]]; then
+  echo "Symlinking dotfiles..."
 
-#   for dotfile in $dotfiles; do
-#     echo "$dotfile"
-#     backup_dotfile $dotfile
-#     symlink_dotfile $dotfile
-#   done
+  for dotfile in $dotfiles; do
+    echo "$dotfile"
+    backup_dotfile $dotfile
+    symlink_dotfile $dotfile
+  done
 
-#   echo "All set! Any existing files were moved to $BACKUPDIR"
-# else
-#   echo "You don't have anything in '$DOTFILEDIR'"
-# fi
+  echo "All set! Any existing files were moved to $BACKUPDIR"
+else
+  echo "You don't have anything in '$DOTFILEDIR'"
+fi
 
 exit 0
