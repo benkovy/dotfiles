8fa9ee2105f126138b5e0c115d084819b3196c7c
Ben Kovacs <ben.kovacs@shopify.com>
2023-07-17 16:10:43 -0300

    manual linking

--- install.sh
+++ install.sh
@@ -6,64 +6,19 @@ if [[ $SPIN ]]; then
   git config --global --unset-all credential.helper
 fi
 
-# if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-#   # install oh my zsh
-#   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# fi
+ln -sf ~/dotfiles/dotfiles/.zshrc ~/.zshrc
 
-# if [[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]]; then
-#   git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-# fi
-
-# if [[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]]; then
-#   git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-# fi
-
-HOMEDIR=$HOME
-BACKUPDIR="$HOMEDIR/.dotfiles.backups"
-CURRENTDIR=$(pwd -P)
-DOTFILEDIR="$CURRENTDIR/dotfiles"
-
-backup_dotfile() {
-  if [[ -e "$HOMEDIR/$1" ]]; then
-
-    # Do we already have any backups? How many?
-    # This needs some work. Doesn't handle things like
-    # $BACKUPDIR/.vim* and $BACKUPDIR/.vimrc* well
-    dotfile_count=$(find $BACKUPDIR/$1* -maxdepth 0 2> /dev/null | wc -l | sed 's/ //g')
-
-    if [[ $dotfile_count -ne '0' ]]; then
-      mv $HOMEDIR/$1 $BACKUPDIR/$1.$dotfile_count
-    else
-      mv $HOMEDIR/$1 $BACKUPDIR/$1
-    fi
-  fi
-}
-
-symlink_dotfile() {
-  ln -sf $DOTFILEDIR/$1 $HOMEDIR/$1
-}
-
-
-if [[ ! -e $BACKUPDIR ]]; then
-  echo "Creating back ups folder $BACKUPDIR..."
-  mkdir $BACKUPDIR
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+  # install oh my zsh
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-dotfiles=$(ls -1 -A $DOTFILEDIR 2> /dev/null)
+if [[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+fi
 
-if [[ $dotfiles ]]; then
-  echo "Symlinking dotfiles..."
-
-  for dotfile in $dotfiles; do
-    echo "$dotfile"
-    backup_dotfile $dotfile
-    symlink_dotfile $dotfile
-  done
-
-  echo "All set! Any existing files were moved to $BACKUPDIR"
-else
-  echo "You don't have anything in '$DOTFILEDIR'"
+if [[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
 
 exit 0
