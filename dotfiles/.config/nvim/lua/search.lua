e9ff49a778166b1e83f77165e46556aa990aecc2
Ben Kovacs <ben.kovacs@shopify.com>
2023-09-18 10:10:42 -0300

    Adjust telescope to use fzf

--- dotfiles/.config/nvim/lua/search.lua
+++ dotfiles/.config/nvim/lua/search.lua
@@ -1,26 +1,41 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.3',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local telescope = require('telescope')
-    local builtin = require('telescope.builtin')
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make'
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.3',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local telescope = require('telescope')
+      local builtin = require('telescope.builtin')
 
-    vim.keymap.set('n', '<leader>F', function()
-      builtin.find_files({ hidden = true })
-    end, {})
-    vim.keymap.set('n', '<leader>f', function()
-      builtin.live_grep({ hidden = true })
-    end, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>F', function()
+        builtin.find_files({ hidden = true })
+      end, {})
+      vim.keymap.set('n', '<leader>f', function()
+        builtin.live_grep({ hidden = true })
+      end, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-    telescope.setup({
-      defaults = {
-        file_ignore_patterns = {
-          "node_modules", "build", "dist", "yarn.lock", "^.git/"
+      telescope.setup({
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules", "build", "dist", "yarn.lock", "^.git/"
+          },
         },
-      }
-    })
-  end
+        extensions = {
+          fzf = {
+            fuzzy = true,                   -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true,    -- override the file sorter
+            case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+          }
+        }
+      })
+      telescope.load_extension("fzf")
+    end
+  }
 }
