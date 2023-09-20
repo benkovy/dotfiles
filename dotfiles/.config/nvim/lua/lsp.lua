675f9177b2a0f1ed4e078f9a1499636f90b292f6
Ben Kovacs <ben.kovacs@shopify.com>
2023-09-20 01:50:42 +0000

    Clean up null-ls

--- dotfiles/.config/nvim/lua/lsp.lua
+++ dotfiles/.config/nvim/lua/lsp.lua
@@ -1,87 +1,87 @@
 return {
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    dependencies = {
-      { 'neovim/nvim-lspconfig' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
-      { "jay-babu/mason-null-ls.nvim" },
-      { "jose-elias-alvarez/null-ls.nvim" },
-      { 'rafamadriz/friendly-snippets' }, -- Optional
-    },
-    config = function()
-      local lsp = require('lsp-zero')
-      lsp.preset('recommended')
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		dependencies = {
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "jay-babu/mason-null-ls.nvim" },
+			{ "jose-elias-alvarez/null-ls.nvim" },
+			{ "rafamadriz/friendly-snippets" }, -- Optional
+		},
+		config = function()
+			local lsp = require("lsp-zero")
+			lsp.preset("recommended")
 
-      lsp.nvim_workspace()
+			lsp.nvim_workspace()
 
-      lsp.set_server_config({
-        on_init = function(client)
-          client.server_capabilities.semanticTokensProvider = nil
-        end,
-      })
+			lsp.set_server_config({
+				on_init = function(client)
+					client.server_capabilities.semanticTokensProvider = nil
+				end,
+			})
 
-      lsp.ensure_installed({
-        'tsserver',
-        'eslint',
-        'graphql',
-        'lua_ls'
-      })
+			lsp.ensure_installed({
+				"tsserver",
+				"eslint",
+				"graphql",
+				"lua_ls",
+			})
 
-      lsp.format_on_save({
-        format_opts = {
-          async = false,
-          timeout_ms = 10000,
-        },
-        servers = {
-          ['lua_ls'] = { 'lua' },
-        }
-      })
+			lsp.format_on_save({
+				format_opts = {
+					async = false,
+					timeout_ms = 10000,
+				},
+				servers = {
+					["null-ls"] = { "javascript", "typescript", "typescriptreact", "javascriptreact", "lua" },
+				},
+			})
 
-      lsp.on_attach(function(client, bufnr)
-        lsp.default_keymaps({ buffer = bufnr })
+			lsp.on_attach(function(client, bufnr)
+				lsp.default_keymaps({ buffer = bufnr })
 
-        local opts = { buffer = bufnr }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+				local opts = { buffer = bufnr }
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 
-        vim.api.nvim_create_autocmd("CursorHold", {
-          buffer = bufnr,
-          callback = function()
-            local opts = {
-              focusable = false,
-              close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-              border = 'rounded',
-              source = 'always',
-              prefix = ' ',
-              scope = 'cursor',
-            }
-            vim.diagnostic.open_float(nil, opts)
-          end
-        })
-      end)
+				vim.api.nvim_create_autocmd("CursorHold", {
+					buffer = bufnr,
+					callback = function()
+						local opts = {
+							focusable = false,
+							close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+							border = "rounded",
+							source = "always",
+							prefix = " ",
+							scope = "cursor",
+						}
+						vim.diagnostic.open_float(nil, opts)
+					end,
+				})
+			end)
 
-      lsp.setup()
+			lsp.setup()
 
-      local null_ls = require('null-ls')
+			local null_ls = require("null-ls")
 
-      null_ls.setup({
-        sources = {
-          -- Replace these with the tools you want to install
-          -- make sure the source name is supported by null-ls
-          -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
-          null_ls.builtins.formatting.prettier,
-          null_ls.builtins.formatting.stylua,
-        }
-      })
+			null_ls.setup({
+				sources = {
+					-- Replace these with the tools you want to install
+					-- make sure the source name is supported by null-ls
+					-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.stylua,
+				},
+			})
 
-      -- See mason-null-ls.nvim's documentation for more details:
-      -- https://github.com/jay-babu/mason-null-ls.nvim#setup
-      require('mason-null-ls').setup({
-        ensure_installed = nil,
-        automatic_installation = true,
-      })
-    end
-  },
+			-- See mason-null-ls.nvim's documentation for more details:
+			-- https://github.com/jay-babu/mason-null-ls.nvim#setup
+			require("mason-null-ls").setup({
+				ensure_installed = nil,
+				automatic_installation = true,
+			})
+		end,
+	},
 }
